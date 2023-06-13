be03708047519e40572747a16546991110eb6502
Michael Petroff <petroff.mb@gmail.com>
2023-06-13 23:06:45 +0500

    Add systemd service file
    clean up

---
+++ ytank/payload.py
@@ -0,0 +1,38 @@
+import json
+import random
+
+import requests
+from lorem.text import TextLorem
+
+CREATE_COUNT = 100
+URL = '/notes/'
+TAG = 'Normal_Requests'
+ # GET||/url||case_tag||body(optional)
+TEMPL_POST = '{}||{}||{}||{}\n'
+TEMPL_GET = '{}||{}||{}\n'
+
+if __name__ == '__main__':
+    # create note
+    lorem = TextLorem(srange=(3, 50))
+    with open('./data.txt', 'wt', encoding='utf-8') as f:
+
+        # for n, i in enumerate(range(CREATE_COUNT)):
+        #     # res = requests.post(
+        #     #         url=URL,
+        #     #         json={'text': lorem.sentence()}
+        #     # )
+        #     body = json.dumps({'text': lorem.sentence()})
+        #     line = TEMPL_POST.format('POST', URL, TAG, body)
+        #     f.write(line)
+
+        # update note
+        for i in range(CREATE_COUNT):
+            # idx = random.randint(0, CREATE_COUNT)
+            # url = f'{URL}{idx}/'
+            # print(idx, url, end=': ')
+            # resp = requests.patch(
+            #         url=url,
+            #         json={'completed': True}
+            # )
+            line = TEMPL_GET.format('GET', URL, TAG)
+            f.write(line)
